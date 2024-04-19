5de27afc6856fba320c63287ea8b15fd051204ed
codemohitpra03 <codemohitprakanojia2103@gmail.com>
2024-04-20 00:02:23 +0530

    Modified Home route - added github repo link and live link to home route for reference

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send('User registration and authentication mechanism - submission by Mohit Kanojia - Monter')
+    res.send(`<h1>User registration and authentication mechanism - submission by Mohit Kanojia - Monter</h1>
+    <br>
+    <h3><a href="https://github.com/codemohitpra03/user-authentication-monter" target="_blank">Github Repo of Assignment</a></h3>
+    <h3><a href="https://user-authentication-monter-mohit-kanojia.onrender.com" target="_blank">Live Deployed Link</a></h3>
+    <h3><a href="https://github.com/codemohitpra03" target="_blank">Github Account</a></h3>
+    `)
 })
 
 app.use('/auth',authRoute)
